a8d067dd08a76a5092ebc9665b8bc4e2f0487350
iikkap <iikka.pohjola@cs.tamk.fi>
2016-06-06 13:51:09 +0300

    Initial movement
    Basics of movement and gravity scripts done

---
+++ Vko1/Assets/Scripts/Gravity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+	public float gravity;
+	float yVelocity;
+	float xVelocity;
+	Vector2 velocity;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity = GetComponent<PlayerMovement> ().GetVelocity();
+		yVelocity = velocity.y - gravity;
+		velocity = new Vector2 (velocity.x, yVelocity);
+
+		GetComponent<PlayerMovement> ().SetVelocity (velocity);
+	}
+		
+}
--- Vko1/Assets/Scripts/PlayerMovement.cs
+++ Vko1/Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float yVelocity;
+	float xVelocity;
+	public float maxYVelocity;
+	public float jumpPower;
+	Vector2 oldPosition;
+	Vector2 newPosition;
+
 	// Use this for initialization
 	void Start () {
-	
+		yVelocity = 0;
+		xVelocity = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		oldPosition = transform.position;
+		newPosition = oldPosition + GetVelocity ();
+		transform.position = newPosition;
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "jump") {
+			SetVelocity (new Vector2 (0f, jumpPower));
+		}
+	}
+
+	public Vector2 GetVelocity() {
+		return new Vector2 (xVelocity, yVelocity);
+	}
+
+	public void SetVelocity(Vector2 velocity) {
+		xVelocity = velocity.x;
+
+		yVelocity = velocity.y;
 	}
 }
